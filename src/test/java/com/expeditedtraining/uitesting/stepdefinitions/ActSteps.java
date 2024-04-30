b7f73203c23050bb56ca807904e994a957315ab4
Maciej Fec <Maciej.Fec@2itesting.com>
2024-04-30 21:16:30 +0100

    add steps

--- src/test/java/com/expeditedtraining/uitesting/stepdefinitions/ActSteps.java
+++ src/test/java/com/expeditedtraining/uitesting/stepdefinitions/ActSteps.java
@@ -1,16 +1,17 @@
 package com.expeditedtraining.uitesting.stepdefinitions;
 
 import com.expeditedtraining.uitesting.user.actions.ui.SortTable;
-import com.expeditedtraining.uitesting.user.actions.ui.TextOf;
+import com.expeditedtraining.uitesting.user.questions.TextOf;
 import com.expeditedtraining.uitesting.utils.SerenitySessionVariableKeys;
 import io.cucumber.java.en.When;
 import net.serenitybdd.core.Serenity;
 import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Performable;
 
 public class ActSteps {
 
     @When("{user} sort the table by {string} in {string} order")
-    public void userSortsTheTable(Actor user, String by, String order) throws InterruptedException {
+    public void userSortsTheTable(Actor user, String by, String order) {
         // Save values for specified column before sorting
         Serenity
                 .setSessionVariable(SerenitySessionVariableKeys.TABLE_VALUES_BEFORE_SORT)
@@ -24,5 +25,12 @@ public class ActSteps {
         Serenity
                 .setSessionVariable(SerenitySessionVariableKeys.TABLE_VALUES_AFTER_SORT)
                 .to(user.asksFor(TextOf.allTableValuesForColumn(by)));
+    }
+
+    @When("{user} perform a {promptAction}")
+    public void userPerformsAPromptAction(Actor user, Performable performPromptAction) {
+        user.attemptsTo(
+                performPromptAction
+        );
     }
 }
--- src/test/java/com/expeditedtraining/uitesting/stepdefinitions/ArrangeSteps.java
+++ src/test/java/com/expeditedtraining/uitesting/stepdefinitions/ArrangeSteps.java
@@ -1,6 +1,7 @@
 package com.expeditedtraining.uitesting.stepdefinitions;
 
 import com.expeditedtraining.uitesting.user.actions.browser.OpenBrowser;
+import com.expeditedtraining.uitesting.user.actions.ui.TriggerJSPrompt;
 import io.cucumber.java.en.Given;
 import net.serenitybdd.screenplay.Actor;
 
@@ -10,6 +11,13 @@ public class ArrangeSteps {
     public void userIsOnTheInitialPage(Actor user, String pageName) {
         user.attemptsTo(
                 OpenBrowser.on(pageName)
+        );
+    }
+
+    @Given("{user} have triggered a {string}")
+    public void userHasTriggeredAJSPrompt(Actor user, String promptType) {
+        user.attemptsTo(
+                TriggerJSPrompt.ofType(promptType)
         );
     }
 }
