6e4da487ade0486e2dfdbcafbe726c3024c7214f
Maciej Fec <Maciej.Fec@2itesting.com>
2024-05-03 12:16:53 +0100

    initial commit

---
+++ src/test/java/com/expeditedtraining/uitesting/ui/compontents/HTML.java
@@ -0,0 +1,8 @@
+package com.expeditedtraining.uitesting.ui.compontents;
+
+import net.serenitybdd.screenplay.targets.Target;
+
+public class HTML {
+
+    public static final Target ELEMENT_WITH_TAG = Target.the("{0} HTML Element").locatedBy("//{0}");
+}
