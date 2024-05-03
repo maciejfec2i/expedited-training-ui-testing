48bf0174d04da1e6044bf2f16e6ad81d25923302
Maciej Fec <Maciej.Fec@2itesting.com>
2024-05-03 12:17:46 +0100

    add step definitions

--- src/test/java/com/expeditedtraining/uitesting/stepdefinitions/AssertSteps.java
+++ src/test/java/com/expeditedtraining/uitesting/stepdefinitions/AssertSteps.java
@@ -1,13 +1,23 @@
 package com.expeditedtraining.uitesting.stepdefinitions;
 
+import com.expeditedtraining.uitesting.ui.compontents.HTML;
 import com.expeditedtraining.uitesting.ui.pages.JavaScripAlertsPage;
+import com.expeditedtraining.uitesting.user.actions.ui.SwitchTo;
+import com.expeditedtraining.uitesting.user.questions.ElementState;
+import com.expeditedtraining.uitesting.user.questions.NumberOf;
 import com.expeditedtraining.uitesting.user.questions.TextOf;
 import com.expeditedtraining.uitesting.utils.SerenitySessionVariableKeys;
 import com.expeditedtraining.uitesting.utils.comparators.MonetaryValueComparator;
 import io.cucumber.java.en.Then;
 import net.serenitybdd.core.Serenity;
+import net.serenitybdd.core.pages.WebElementState;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.actions.Switch;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.ensure.Ensure;
+import net.serenitybdd.screenplay.ensure.web.ElementLocated;
+import net.serenitybdd.screenplay.waits.WaitUntil;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.Collections;
 import java.util.List;
@@ -43,6 +53,21 @@ public class AssertSteps {
     public void pageShouldDisplayExpectedText(String expectedText) {
         OnStage.theActorInTheSpotlight().attemptsTo(
                 Ensure.that(TextOf.target(JavaScripAlertsPage.ALERT_ACTION_RESULT)).isEqualTo(expectedText)
+        );
+    }
+
+    @Then("a new tab should be opened")
+    public void newTabShouldBeOpened() {
+        OnStage.theActorInTheSpotlight().attemptsTo(
+                Ensure.that(NumberOf.openTabs()).isEqualTo(2)
+        );
+    }
+
+    @Then("the {user} should be able to switch to the tab and see the page content")
+    public void userShouldBeAbleToSwitchToTabAndSeePageContent(Actor user) {
+        user.attemptsTo(
+                SwitchTo.aNewWindow(),
+                Ensure.that(ElementLocated.by((HTML.ELEMENT_WITH_TAG.of("html")))).isDisplayed()
         );
     }
 }
