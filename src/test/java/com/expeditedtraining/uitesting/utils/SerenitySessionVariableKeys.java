2257b969284a36d4dace3e5131a353134ca7838a
Maciej Fec <Maciej.Fec@2itesting.com>
2024-05-06 11:47:34 +0100

    add key for user credentials

--- src/test/java/com/expeditedtraining/uitesting/utils/SerenitySessionVariableKeys.java
+++ src/test/java/com/expeditedtraining/uitesting/utils/SerenitySessionVariableKeys.java
@@ -1,6 +1,8 @@
 package com.expeditedtraining.uitesting.utils;
 
 public class SerenitySessionVariableKeys {
+
+    public static String USER_CREDENTIALS = "userCredentials";
     public static String TABLE_VALUES_BEFORE_SORT = "tableValuesBeforeSort";
     public static String TABLE_VALUES_AFTER_SORT = "tableValuesAfterSort";
 }
