05ba13943604a417762863afd7b26c23b9849705
Maciej Fec <Maciej.Fec@2itesting.com>
2024-05-17 09:02:23 +0100

    deleted as part of refactor

--- src/test/java/com/expeditedtraining/uitesting/ui/pages/JavaScripAlertsPage.java
+++
@@ -1,8 +0,0 @@
-package com.expeditedtraining.uitesting.ui.pages;
-
-import net.serenitybdd.screenplay.targets.Target;
-
-public class JavaScripAlertsPage {
-
-    public static final Target ALERT_ACTION_RESULT = Target.the("JS Alert Action Result").locatedBy("#result");
-}
