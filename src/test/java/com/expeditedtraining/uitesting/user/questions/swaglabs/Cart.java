e7c4c010c5cccc5e275650cfd984aa8d9fb62e29
Maciej Fec <Maciej.Fec@2itesting.com>
2024-05-20 16:53:23 +0100

    update to use abstracted question class

--- src/test/java/com/expeditedtraining/uitesting/user/questions/swaglabs/Cart.java
+++ src/test/java/com/expeditedtraining/uitesting/user/questions/swaglabs/Cart.java
@@ -1,16 +1,14 @@
-package com.expeditedtraining.uitesting.user.questions;
+package com.expeditedtraining.uitesting.user.questions.swaglabs;
 
-import com.expeditedtraining.uitesting.ui.compontents.ItemCard;
 import net.serenitybdd.screenplay.Question;
-import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 
 import java.util.List;
 
 public class Cart {
 
     public static Question<List<String>> items() {
-        return Question.about("Names of all items in the cart").answeredBy(
-                actor -> BrowseTheWeb.as(actor).findAll(ItemCard.ITEM_NAME).textContents()
-        );
+        return Question
+                .about("Names of all the items in the cart")
+                .answeredBy(new NamesOfAllCartItems());
     }
 }
