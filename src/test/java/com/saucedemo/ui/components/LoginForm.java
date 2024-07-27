ec5754d89d9ea1d85df7fb58553dfa5ed86ecce5
Maciej Fec <Maciej.Fec@2itesting.com>
2024-07-27 09:35:39 +0100

    fix: add css: prefix for locators to prevent serenity from treating them as xpaths

--- src/test/java/com/saucedemo/ui/components/LoginForm.java
+++ src/test/java/com/saucedemo/ui/components/LoginForm.java
@@ -4,7 +4,7 @@ import net.serenitybdd.screenplay.targets.Target;
 
 public class LoginForm {
 
-    public static final Target USERNAME_FIELD = Target.the("login form username field").locatedBy("input[data-test = 'username']");
-    public static final Target PASSWORD_FIELD = Target.the("login form password field").locatedBy("input[data-test = 'password']");
-    public static final Target LOGIN_BUTTON = Target.the("login button").locatedBy("input[data-test = 'login-button']");
+    public static final Target USERNAME_FIELD = Target.the("login form username field").locatedBy("css:input[data-test = 'username']");
+    public static final Target PASSWORD_FIELD = Target.the("login form password field").locatedBy("css:input[data-test = 'password']");
+    public static final Target LOGIN_BUTTON = Target.the("login button").locatedBy("css:input[data-test = 'login-button']");
 }
