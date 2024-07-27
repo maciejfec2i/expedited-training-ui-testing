4bef80d3a53b1130270811f01ae04d321f421165
Maciej Fec <Maciej.Fec@2itesting.com>
2024-07-27 09:31:59 +0100

    feat: add method to retrieve url of current page

--- src/test/java/com/saucedemo/actor/questions/Url.java
+++ src/test/java/com/saucedemo/actor/questions/Url.java
@@ -1,5 +1,6 @@
 package com.saucedemo.actor.questions;
 
+import com.saucedemo.actor.questions.url.UrlOfCurrentPage;
 import com.saucedemo.actor.questions.url.UrlOfPageObject;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.screenplay.Question;
@@ -10,5 +11,9 @@ public class Url {
         return Question
                 .about("The URL of " + pageObject.getClass().getSimpleName())
                 .answeredBy(new UrlOfPageObject(pageObject));
+    }
+
+    public static Question<String> ofTheCurrentlyOpenPage() {
+        return Question.about("The URL of the currently open page").answeredBy(new UrlOfCurrentPage());
     }
 }
