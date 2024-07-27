4c404e677a07df00f5ddf07e091b2fc3a82611c2
Maciej Fec <Maciej.Fec@2itesting.com>
2024-07-27 09:34:47 +0100

    feat: update to extend BasePageObject

--- src/test/java/com/saucedemo/ui/pages/SwagLabsInventoryPage.java
+++ src/test/java/com/saucedemo/ui/pages/SwagLabsInventoryPage.java
@@ -1,8 +1,7 @@
 package com.saucedemo.ui.pages;
 
 import net.serenitybdd.annotations.DefaultUrl;
-import net.serenitybdd.core.pages.PageObject;
 
 @DefaultUrl("swag.labs.inventory.page.url")
-public class SwagLabsInventoryPage extends PageObject {
+public class SwagLabsInventoryPage extends BasePageObject {
 }
--- src/test/java/com/saucedemo/ui/pages/SwagLabsLoginPage.java
+++ src/test/java/com/saucedemo/ui/pages/SwagLabsLoginPage.java
@@ -1,8 +1,7 @@
 package com.saucedemo.ui.pages;
 
 import net.serenitybdd.annotations.DefaultUrl;
-import net.serenitybdd.core.pages.PageObject;
 
 @DefaultUrl("swag.labs.login.page.url")
-public class SwagLabsLoginPage extends PageObject {
+public class SwagLabsLoginPage extends BasePageObject {
 }
