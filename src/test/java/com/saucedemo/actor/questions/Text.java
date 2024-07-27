aad1dd0501328a05c6e6aca2b8e320688741b298
Maciej Fec <Maciej.Fec@2itesting.com>
2024-07-27 12:44:12 +0100

    feat: add method to retrieve text of all targets located by the provided target locator

--- src/test/java/com/saucedemo/actor/questions/Text.java
+++ src/test/java/com/saucedemo/actor/questions/Text.java
@@ -1,12 +1,19 @@
 package com.saucedemo.actor.questions;
 
+import com.saucedemo.actor.questions.text.TextOfEachTarget;
 import com.saucedemo.actor.questions.text.TextOfTarget;
 import net.serenitybdd.screenplay.Question;
 import net.serenitybdd.screenplay.targets.Target;
+
+import java.util.Collection;
 
 public class Text {
 
     public static Question<String> of(Target targetElement) {
         return Question.about("the text of the " + targetElement).answeredBy(new TextOfTarget(targetElement));
+    }
+
+    public static Question<Collection<String>> ofEach(Target targetElement) {
+        return Question.about("the text of each " + targetElement).answeredBy(new TextOfEachTarget(targetElement));
     }
 }
