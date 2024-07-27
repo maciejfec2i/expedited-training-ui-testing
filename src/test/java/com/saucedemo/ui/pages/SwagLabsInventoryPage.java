49823cac66b71a93f655a6fbe8bb0fcf8cb4540a
Maciej Fec <Maciej.Fec@2itesting.com>
2024-07-27 12:43:08 +0100

    feat: add locator for the product sort dropdown

--- src/test/java/com/saucedemo/ui/pages/SwagLabsInventoryPage.java
+++ src/test/java/com/saucedemo/ui/pages/SwagLabsInventoryPage.java
@@ -1,7 +1,12 @@
 package com.saucedemo.ui.pages;
 
 import net.serenitybdd.annotations.DefaultUrl;
+import net.serenitybdd.screenplay.targets.Target;
 
 @DefaultUrl("swag.labs.inventory.page.url")
 public class SwagLabsInventoryPage extends BasePageObject {
+
+    public static final Target PRODUCT_SORT_DROPDOWN = Target
+            .the("product sort dropdown")
+            .locatedBy("css:select[data-test = 'product-sort-container']");
 }
