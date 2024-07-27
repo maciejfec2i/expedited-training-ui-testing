cbc6e76fc0318cfd2ea22e348480e6b410122de3
Maciej Fec <Maciej.Fec@2itesting.com>
2024-07-27 09:34:07 +0100

    feat: add 2 new param definitions

--- src/test/java/com/saucedemo/stepdefinitions/ParameterDefinitions.java
+++ src/test/java/com/saucedemo/stepdefinitions/ParameterDefinitions.java
@@ -1,6 +1,10 @@
 package com.saucedemo.stepdefinitions;
 
+import com.saucedemo.data.ErrorMessage;
+import com.saucedemo.ui.pages.BasePageObject;
+import com.saucedemo.ui.pages.factories.PageObjectFactory;
 import io.cucumber.java.ParameterType;
+import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.actors.OnStage;
 
@@ -9,5 +13,15 @@ public class ParameterDefinitions {
     @ParameterType(".*")
     public Actor actor(String actorName) {
         return OnStage.theActorCalled(actorName);
+    }
+
+    @ParameterType(".*")
+    public PageObject page(String pageName) {
+        return PageObjectFactory.from(BasePageObject.class.getPackage()).getPageCalled(pageName + "Page");
+    }
+
+    @ParameterType(".*")
+    public ErrorMessage error(String errorMessageName) {
+        return ErrorMessage.valueOf(errorMessageName.toUpperCase().replace(" ", "_"));
     }
 }
