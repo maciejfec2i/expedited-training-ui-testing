47b8e31d4167dd076c8144fbef495bd3d753fab1
Maciej Fec <Maciej.Fec@2itesting.com>
2024-07-30 13:57:33 +0100

    chore: update enum constant name to reflect item name update

--- src/test/java/com/saucedemo/data/InventoryItem.java
+++ src/test/java/com/saucedemo/data/InventoryItem.java
@@ -5,7 +5,7 @@ public enum InventoryItem {
     SAUCE_LABS_BIKE_LIGHT("Sauce Labs Bike Light", 0),
     SAUCE_LABS_BOLT_TSHIRT("Sauce Labs Bolt T-Shirt", 1),
     SAUCE_LABS_ONESIE("Sauce Labs Onesie", 2),
-    TESTALLTHETHINGS_TSHIRT("Test.allTheThings() T-Shirt", 3),
+    TESTALLTHETHINGS_TSHIRT_RED("Test.allTheThings() T-Shirt (Red)", 3),
     SAUCE_LABS_BACKPACK("Sauce Labs Backpack", 4),
     SAUCE_LABS_FLEECE_JACKET("Sauce Labs Fleece Jacket", 5);
 
