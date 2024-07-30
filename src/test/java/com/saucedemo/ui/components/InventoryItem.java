d9a4de198f321d908201543c135c1d419820cd21
Maciej Fec <Maciej.Fec@2itesting.com>
2024-07-30 13:54:15 +0100

    chore: update locator access modifiers

--- src/test/java/com/saucedemo/ui/components/InventoryItem.java
+++ src/test/java/com/saucedemo/ui/components/InventoryItem.java
@@ -4,11 +4,11 @@ import net.serenitybdd.screenplay.targets.Target;
 
 public class InventoryItem {
 
-    private static final Target INVENTORY_ITEM_NAME = Target.the("inventory item name").locatedBy("css:div[data-test='inventory-item-name']");
-    private static final Target INVENTORY_ITEM_PRICE = Target.the("inventory item price").locatedBy("css:div[data-test='inventory-item-price']");
     private static final String INVENTORY_ITEM_CARD_OF = "//div[@data-test = 'inventory-item' and descendant::div[contains(normalize-space(), '{0}')]]";
 
     public final static Target CARD = Target.the("inventory item card").locatedBy("css:div[data-test = 'inventory-item']");
+    public static final Target NAME = Target.the("inventory item name").locatedBy("css:div[data-test='inventory-item-name']");
+    public static final Target PRICE = Target.the("inventory item price").locatedBy("css:div[data-test='inventory-item-price']");
     public static final Target ADD_TO_CART_BUTTON = Target.the("Add to cart button on the {0} item card").locatedBy(INVENTORY_ITEM_CARD_OF + "//button[contains(normalize-space(), 'Add to cart')]");
     public static final Target REMOVE_FROM_CART_BUTTON = Target.the("Remove button on the {0} item card").locatedBy(INVENTORY_ITEM_CARD_OF + "//button[contains(normalize-space(), 'Remove')]");
 
@@ -16,8 +16,8 @@ public class InventoryItem {
     public static Target componentBasedOn(String sortFilter) {
         String sortFilterName = sortFilter.toLowerCase();
 
-        if(sortFilterName.toLowerCase().contains("name")) return INVENTORY_ITEM_NAME;
-        else if(sortFilterName.toLowerCase().contains("price")) return INVENTORY_ITEM_PRICE;
+        if(sortFilterName.toLowerCase().contains("name")) return NAME;
+        else if(sortFilterName.toLowerCase().contains("price")) return PRICE;
         else throw new RuntimeException(String.format("%s is not a recognised sort filter", sortFilter));
     }
 
