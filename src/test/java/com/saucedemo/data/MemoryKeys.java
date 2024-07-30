d00547fb8c9119783c82584f0daf8d5a6a9834d2
Maciej Fec <Maciej.Fec@2itesting.com>
2024-07-30 12:09:07 +0100

    chore: add keys

--- src/test/java/com/saucedemo/data/MemoryKeys.java
+++ src/test/java/com/saucedemo/data/MemoryKeys.java
@@ -4,4 +4,6 @@ public class MemoryKeys {
 
     public static final String ITEM_VALUES_BEFORE_SORT = "itemValuesBeforeSort";
     public static final String ITEM_VALUES_AFTER_SORT = "itemValuesAfterSort";
+    public static final String ITEMS_ADDED_TO_CART = "itemsAddedToCart";
+    public static final String ITEMS_REMOVED_FROM_CART = "itemsRemovedFromCart";
 }
