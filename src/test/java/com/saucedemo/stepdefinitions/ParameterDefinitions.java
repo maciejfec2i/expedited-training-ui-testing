d92580c4bd564af3e50ed1ffde94f55288a5abce
Maciej Fec <Maciej.Fec@2itesting.com>
2024-07-30 12:10:00 +0100

    feat: add new parameter definition

--- src/test/java/com/saucedemo/stepdefinitions/ParameterDefinitions.java
+++ src/test/java/com/saucedemo/stepdefinitions/ParameterDefinitions.java
@@ -1,13 +1,19 @@
 package com.saucedemo.stepdefinitions;
 
+import com.google.common.base.Strings;
 import com.saucedemo.data.ErrorMessage;
 import com.saucedemo.data.SortOrder;
+import com.saucedemo.ui.components.InventoryItem;
 import com.saucedemo.ui.pages.BasePageObject;
 import com.saucedemo.ui.pages.factories.PageObjectFactory;
 import io.cucumber.java.ParameterType;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.targets.Target;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParameterDefinitions {
 
@@ -29,5 +35,19 @@ public class ParameterDefinitions {
     @ParameterType(".*")
     public SortOrder order(String sortOrderName) {
         return SortOrder.valueOf(sortOrderName.toUpperCase().replace(" ", "_"));
+    }
+
+    @ParameterType(".*")
+    public List<String> items(String itemNames) {
+        return Strings.isNullOrEmpty(itemNames) ? new ArrayList<>() : List.of(itemNames.split(", "));
+    }
+
+    @ParameterType(".*")
+    public Target button(String buttonText) {
+        String lowerCaseButtonText = buttonText.toLowerCase();
+
+        if(lowerCaseButtonText.contains("add")) return InventoryItem.ADD_TO_CART_BUTTON;
+        else if(lowerCaseButtonText.contains("remove")) return InventoryItem.REMOVE_FROM_CART_BUTTON;
+        else throw new RuntimeException();
     }
 }
