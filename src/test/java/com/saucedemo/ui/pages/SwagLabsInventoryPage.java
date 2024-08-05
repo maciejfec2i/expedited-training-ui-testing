b06dc735f74b2c90ccd0581cfe69af61100043ea
Maciej Fec <Maciej.Fec@2itesting.com>
2024-08-05 20:02:49 +0100

    feat: add some POM examples

--- src/test/java/com/saucedemo/ui/pages/SwagLabsInventoryPage.java
+++ src/test/java/com/saucedemo/ui/pages/SwagLabsInventoryPage.java
@@ -2,6 +2,10 @@ package com.saucedemo.ui.pages;
 
 import net.serenitybdd.annotations.DefaultUrl;
 import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
+
+import java.util.List;
+import java.util.function.Function;
 
 @DefaultUrl("swag.labs.inventory.page.url")
 public class SwagLabsInventoryPage extends BasePageObject {
@@ -9,4 +13,27 @@ public class SwagLabsInventoryPage extends BasePageObject {
     public static final Target PRODUCT_SORT_DROPDOWN = Target
             .the("product sort dropdown")
             .locatedBy("css:select[data-test = 'product-sort-container']");
+
+    private final Function <String, String> ITEM_CARD_LOCATOR_FOR_ITEM = itemName -> String.format(
+            "//div[@data-test = 'inventory-item' and descendant::div[contains(normalize-space(), '%s')]]", itemName
+    );
+    private final Function<String, By> ADD_TO_CART_BUTTON_FOR_ITEM = itemName -> By.xpath(
+            String.format("%s//button[contains(normalize-space(), 'Add to cart')]", ITEM_CARD_LOCATOR_FOR_ITEM.apply(itemName))
+    );
+
+    private final Function<String, By> REMOVE_FROM_CART_BUTTON_FOR_ITEM = itemName -> By.xpath(
+            String.format("%s//button[contains(normalize-space(), 'Remove')]", ITEM_CARD_LOCATOR_FOR_ITEM.apply(itemName))
+    );
+
+    public void addToCartTheFollowing(List<String> items) {
+        for(String itemName : items) {
+            clickOnTheElementLocatedBy(ADD_TO_CART_BUTTON_FOR_ITEM.apply(itemName));
+        }
+    }
+
+    public void removeFromCartTheFollowing(List<String> items) {
+        for(String itemName : items) {
+            clickOnTheElementLocatedBy(REMOVE_FROM_CART_BUTTON_FOR_ITEM.apply(itemName));
+        }
+    }
 }
--- src/test/java/com/saucedemo/ui/pages/SwagLabsLoginPage.java
+++ src/test/java/com/saucedemo/ui/pages/SwagLabsLoginPage.java
@@ -1,7 +1,20 @@
 package com.saucedemo.ui.pages;
 
+import com.saucedemo.data.Credentials;
 import net.serenitybdd.annotations.DefaultUrl;
+import org.openqa.selenium.By;
 
 @DefaultUrl("swag.labs.login.page.url")
 public class SwagLabsLoginPage extends BasePageObject {
+
+    private final By USERNAME_INPUT = By.cssSelector("input[data-test = 'username']");
+    private final By PASSWORD_INPUT = By.cssSelector("input[data-test = 'password']");
+    private final By LOGIN_BTN = By.cssSelector("input[data-test = 'login-button']");
+
+    public void loginUsing(Credentials credentials) {
+        openUrl();
+        enterTextIntoElementLocatedBy(USERNAME_INPUT, credentials.username());
+        enterTextIntoElementLocatedBy(PASSWORD_INPUT, credentials.password());
+        clickOnTheElementLocatedBy(LOGIN_BTN);
+    }
 }
